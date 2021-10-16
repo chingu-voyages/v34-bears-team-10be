b85140c581f4efeed37c7d40c82be150250ee1c4
Dasha V <29864815+dashaverbina@users.noreply.github.com>
2021-10-16 03:52:39 +0300

    Set up project structure

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { Sequelize } from "sequelize";
+
+import productsRoutes from "./components/products/productsRoutes.js";
 
 const app = express()
-dotenv.config()
 
-const port = process.env.PORT
+dotenv.config()
+const { PORT, CONNECTION_URI } = process.env
+
+
+const sequelize = new Sequelize(CONNECTION_URI, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+})
+
+
+try {
+  await sequelize.authenticate();
+  console.log("Connection has been established")
+} catch (error) {
+  console.log('Unable to connect to the database:', error)
+} finally {
+  sequelize.close()
+}
+
+app.use('/products', productsRoutes)
 
 app.get('/status', (req, res) => {
   res.status(200).end()
 })
 
-app.listen(port, ()=> {
-  console.log(`App listening at ${port}`)
+app.listen(PORT, ()=> {
+  console.log(`App listening at ${PORT}`)
 })
